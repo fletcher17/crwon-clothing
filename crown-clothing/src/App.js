a0c2ab996dad16fe972fccf9b463097484423f58
fletcher17 <ororo17@gmail.com>
2020-08-05 10:47:22 +0100

    collection

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Homepage from './Pages/homepage/homepage.component';
+import ShopPage from './Pages/shop-page/shop.component';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
--- crown-clothing/src/Components/menu-item/menu-item.component.jsx
+++ crown-clothing/src/Components/menu-item/menu-item.component.jsx
@@ -6,7 +6,7 @@ import './menu-item.styles.scss';
 
 const MenuItem = ({title, imageUrl, size}) => {
 	return (
-		    <div className={`${size} menu-item`} >
+		    <div className={`${size} menu-item`}>
 		      <div className='background-image' 
 		      	   style={{ backgroundImage:`url(${imageUrl}`}} />
 		  	  <div className='content'>
