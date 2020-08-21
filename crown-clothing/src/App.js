4adce7ba6f9bfa518c67bc26296cb4ac34bb4ea1
fletcher17 <ororo17@gmail.com>
2020-08-21 15:24:25 +0100

    implemented firebase firestore

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -4,7 +4,7 @@ import ShopPage from './Pages/shop-page/shop.component';
 import { Switch, Route } from 'react-router-dom';
 import Header from './Components/header/header.component';
 import SignInAndSignUp from './Pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import './App.css';
 
 class App extends Component {
@@ -19,9 +19,22 @@ class App extends Component {
     unsubscribeFromAuth= null;
 
   componentDidMount () {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged (user => 
-      { this.setState({ currentUser: user })
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged (async userAuth => {
+      if (userAuth) {
+       const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState ({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        });
+      } else {
+        this.setState({currentUser: userAuth});
+      }
+    });
   };
 
   componentWillUnmount () {
--- crown-clothing/src/firebase/firebase.utils.js
+++ crown-clothing/src/firebase/firebase.utils.js
@@ -11,7 +11,33 @@ const config = {
     storageBucket: "crwn-clothing-39e69.appspot.com",
     messagingSenderId: "730133873327",
     appId: "1:730133873327:web:e0958957780b0f72860a5a",
-    measurementId: "G-CHPDSLMWT7" };
+    measurementId: "G-CHPDSLMWT7" 
+};
+
+
+export const createUserProfileDocument = 
+    async (userAuth, additionalData) => {
+        if (!userAuth) return;
+
+        const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+        const snapShot = await userRef.get();
+
+        if(!snapShot.exists) {
+            const {displayName, email} = userAuth;
+            const createdAt = new Date();
+
+            try {
+                await userRef.set ({
+                    displayName, email, createdAt, ...additionalData
+                });
+            } catch (error) {
+                        console.log ('error creating user', error.message);
+                    }
+        }
+
+    }
+    
 
     firebase.initializeApp(config);
 
